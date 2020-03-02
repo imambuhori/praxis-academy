70b549a6d9103fb193cc7f52f162e227391e27a1
imamBuhori <imambuchori23.ib@gmail.com>
2020-03-02 16:48:35 +0700

    test

---
+++ Novice/01-01/latihan/Penjumlahan.java
@@ -0,0 +1,9 @@
+public class Penjumlahan{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 11;
+        int c = a+b;
+        System.out.print("Hasil dari " + a + "+" + b + " adalah " + c);
+
+    }
+}
