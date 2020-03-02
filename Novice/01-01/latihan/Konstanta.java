20a7d31694cdaa5f13f13d9c398f88f0bea7cf29
imamBuhori <imambuchori23.ib@gmail.com>
2020-03-02 22:11:55 +0700

    Konstanta

---
+++ Novice/01-01/latihan/Konstanta.java
@@ -0,0 +1,10 @@
+public class Konstanta{
+    public static void main(String[] args){
+        final double PI = 3.1459; //deklarasi Konstanta
+
+        double radius = 20;
+	double luas = radius * radius * PI;
+
+	System.out.println("Luas lingkaran dengan radius " + radius + " adalah " + luas); 
+    }
+}
